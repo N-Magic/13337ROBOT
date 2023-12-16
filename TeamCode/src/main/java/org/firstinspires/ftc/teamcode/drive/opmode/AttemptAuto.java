53d9b921e3e4d9fc08c1e5c4ffd62952d7212796
signal86 <diafakemail@gmail.com>
2023-12-15 21:37:44 -0600

    goated progress still not tuned    :D :D :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AttemptAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AttemptAuto.java
@@ -13,38 +13,69 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
  */
 @Autonomous(group = "drive")
 public class AttemptAuto extends LinearOpMode {
+
+    private double correctionInch = (95 / 60); // kms (Still untuned even with StraightTest, nothing is accurate)
+
+    private void clawToggle(Boolean open) {
+        if (open) {
+            // open claw
+        } else {
+            // close claw
+        }
+    }
+
+    private void armPosition(int deg) {
+        //
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
+
         waitForStart();
+
 
         drive.setPoseEstimate(new Pose2d(10, 10, Math.toRadians(90)));
 
 
-
         Trajectory traj = drive.trajectoryBuilder(new Pose2d())
-                .splineTo(new Vector2d(30, 30), 0)
+                .forward(25 * correctionInch)
                 .build();
+        drive.followTrajectory(traj);
+        sleep(2000);
+        clawToggle(true); // open claw
+//        armPosition(15); toggle hover mode
+        sleep(2000);
+        clawToggle(false); // close claw
 
-        Trajectory D12 = drive.trajectoryBuilder(new Pose2d())
-                .splineToConstantHeading(new Vector2d(50, 50), Math.toRadians(90))
-                .build();
+        sleep(2000);
+//        traj = drive.trajectoryBuilder(new Pose2d())
+//                .splineTo(new Vector2d())
 
-        drive.followTrajectory(D12);
-//        telemetry.addData("Finished first move, pos : " , )
-        sleep(2000);
-        drive.followTrajectory((traj));
-        sleep(2000);
-        drive.followTrajectory(D12);
-//        telemetry.addData("Finished first move, pos : " , )
-        sleep(2000);
-        drive.followTrajectory((traj));
-        sleep(2000);drive.followTrajectory(D12);
-//        telemetry.addData("Finished first move, pos : " , )
-        sleep(2000);
-        drive.followTrajectory((traj));
-        sleep(2000);
+
+//        Trajectory traj = drive.trajectoryBuilder(new Pose2d())
+//                .splineTo(new Vector2d(30, 30), 0)
+//                .build();
+//
+//        Trajectory D12 = drive.trajectoryBuilder(new Pose2d())
+//                .splineToConstantHeading(new Vector2d(50, 50), Math.toRadians(90))
+//                .build();
+//
+//        drive.followTrajectory(D12);
+////        telemetry.addData("Finished first move, pos : " , )
+//        sleep(2000);
+//        drive.followTrajectory((traj));
+//        sleep(2000);
+//        drive.followTrajectory(D12);
+////        telemetry.addData("Finished first move, pos : " , )
+//        sleep(2000);
+//        drive.followTrajectory((traj));
+//        sleep(2000);drive.followTrajectory(D12);
+////        telemetry.addData("Finished first move, pos : " , )
+//        sleep(2000);
+//        drive.followTrajectory((traj));
+//        sleep(2000);
 //        drive.followTrajectory(
 //                drive.trajectoryBuilder(D12.end(), true)
 //                        .splineToConstantHeading(new Vector2d(0, 0), Math.toRadians(90))
